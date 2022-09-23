912f89d7e46a0b6a30aaf50df91f21aa7f70a7b6
nishaya raj <nishayaraj@gmail.com>
2022-09-23 15:01:04 -0500

    updated welcome page UI, Added greetings component

--- pages/index.js
+++ pages/index.js
@@ -1,68 +1,158 @@
+/* eslint-disable @next/next/no-img-element */
+import React, { useEffect, useState, useCallback } from 'react';
+import { useRouter } from 'next/router';
 import { useAuth } from '../utils/context/authContext';
-import HomeCardRow from '../components/HomeCardRow';
-import HomeCard from '../components/HomeCard';
-import MiddleHomeCard from '../components/MiddleHomeCard';
-import TopMiddleHomeCard from '../components/TopMiddleHomeCard';
-import SideMiddleHomeCard from '../components/SideMiddleHomeCard';
+import { getMyStories, getMyJournals } from '../api';
 
 function Home() {
   const { user } = useAuth();
+  const [totalStories, setTotalStories] = useState(0);
+  const [totalJournals, setTotalJournals] = useState(0);
+  const router = useRouter();
+
+  const routeToNewJournalPage = () => router.push('/my-journal/new');
+
+  const routeToNewStoryPage = () => router.push('./my-stories/new');
+
+  const [greetings, setGreetings] = useState({
+    hourGreetings: 'Good morning',
+  });
+
+  const fetchMyStoriesData = useCallback(() => {
+    getMyStories(user.uid).then((stories) => {
+      if (stories?.length > 0) {
+        setTotalStories(stories.length);
+      }
+    });
+  }, [user]);
+
+  useEffect(() => {
+    fetchMyStoriesData();
+  }, [fetchMyStoriesData, user]);
+
+  const fetchMyJournalsData = useCallback(() => {
+    getMyJournals(user.uid).then((journals) => {
+      if (journals?.length > 0) {
+        setTotalJournals(journals.length);
+      }
+    });
+  }, [user]);
+
+  useEffect(() => {
+    fetchMyJournalsData();
+  }, [fetchMyJournalsData, user]);
+
+  useEffect(() => {
+    const currHour = new Date().getHours();
+    let hourGreetings = 'Good evening';
+    if (currHour < 12) {
+      hourGreetings = 'Good morning';
+    } else if (currHour < 18) {
+      hourGreetings = 'Good afternoon';
+    }
+
+    setGreetings({ ...greetings, hourGreetings });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div
       style={{
-        display: 'flex',
+        backgroundImage: 'url("/duskyJournal.jpg")',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
         height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'end',
+        color: 'white',
         flexDirection: 'column',
+        marginTop: '-68px',
       }}
     >
-      <HomeCardRow>
-        <HomeCard />
-        <TopMiddleHomeCard>
-          View journals
-        </TopMiddleHomeCard>
-        <HomeCard />
-      </HomeCardRow>
-      <HomeCardRow>
-        <SideMiddleHomeCard>
-          Add journal
-        </SideMiddleHomeCard>
-        <MiddleHomeCard>
-          <p>Hello {user.displayName}!</p>
-          <p>Come, Weave your Stories with Yarn Momento</p>
-          <p>&quot;Some stories should be out of mind, but not out of sight...&quot;</p>
-        </MiddleHomeCard>
-        <SideMiddleHomeCard>
-          Add stories
-        </SideMiddleHomeCard>
-      </HomeCardRow>
-      <HomeCardRow>
-        <HomeCard />
-        <TopMiddleHomeCard>
-          View stories
-        </TopMiddleHomeCard>
-        <HomeCard />
-      </HomeCardRow>
+      <img
+        style={{
+          borderRadius: '100px',
+          objectFit: 'fill',
+          border: '3px solid white',
+        }}
+        src={user.photoURL}
+        alt={`Hello ${user.displayName}`}
+      />
+      <div
+        style={{
+          fontSize: '32px',
+          marginTop: '20px',
+        }}
+      >
+        {greetings.hourGreetings}, {user.displayName}
+      </div>
+      <div
+        style={{
+          fontSize: '21px',
+          marginTop: '12px',
+        }}
+      >
+        You have {totalStories} {totalStories <= 1 ? 'story' : 'stories' } & {totalJournals} {totalJournals <= 1 ? 'journal' : 'journals' }
+      </div>
+      <div
+        style={{
+          margin: '32px 0px 50px',
+          display: 'flex',
+        }}
+      >
+        <button
+          type="button"
+          onClick={routeToNewJournalPage}
+          style={{
+            display: 'flex',
+            padding: '10px 15px',
+            alignItems: 'center',
+            backgroundColor: '#fff',
+            border: '1px solid',
+            borderColor: '#dadce0',
+            borderRadius: '100px',
+            justifyContent: 'center',
+          }}
+        >
+          <img
+            src="/addJournal.png"
+            alt="Add journal"
+            style={{
+              height: '28px',
+              marginRight: '6px',
+            }}
+          />
+          Add New Journal
+        </button>
+        <button
+          type="button"
+          onClick={routeToNewStoryPage}
+          style={{
+            display: 'flex',
+            padding: '10px 15px',
+            alignItems: 'center',
+            backgroundColor: '#fff',
+            border: '1px solid',
+            borderColor: '#dadce0',
+            borderRadius: '100px',
+            justifyContent: 'center',
+            marginLeft: '20px',
+          }}
+        >
+          <img
+            src="/addStory.png"
+            alt="Add story"
+            style={{
+              height: '28px',
+              marginRight: '6px',
+            }}
+          />
+          Add New Story
+        </button>
+      </div>
     </div>
   );
 }
 
 export default Home;
-
-/** *
- * <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-        fontFamily: 'cursive',
-      }}
-    >
-      <h1>Hello {user.displayName}! </h1>
-      <h3>Come, Weave your Stories with Yarn Momento</h3>
-      <h3>&quot;Some stories should be out of mind, but not out of sight...&quot;</h3>
-    </div>
- *
- */
